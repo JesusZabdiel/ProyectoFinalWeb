f4e5d043bc0b1cef9bdc160dbe27ccd1ae206815
PointedBeatle <a01066812@itesm.mx>
2021-11-28 18:01:19 -0600

    correccion

--- back-end/routes/review.js
+++ back-end/routes/review.js
@@ -1,11 +1,11 @@
-const reviewCtrl = require('../controllers/reviews')
+const reviewCtrl = require('../controllers/reviews') 
 
 const router = require('express').Router()
 
-router.get('reviews' , reviewCtrl.getReviews)
-router.post('addReview' , reviewCtrl.postReview)
-router.post('deleteReview' , reviewCtrl.postDeleteReview)
-router.post('editReview' , reviewCtrl.postEditReview)
+router.get('/reviews' , reviewCtrl.getReviews)
+router.post('/addReview' , reviewCtrl.postReview)
+router.post('/deleteReview' , reviewCtrl.postDeleteReview)
+router.post('/editReview' , reviewCtrl.postEditReview)
 
 
 module.exports = router
